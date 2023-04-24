594e061cd48372f801b2d774b02861b06711be8b
Ololade0 <adesuyiololade@gmail.com>
2023-04-24 15:55:38 +0100

    BlogWebApp

--- Blog/urls.py
+++ Blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.blogs, name="blogs"),
     path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
     path("add_blogs/", views.add_blogs, name="add_blogs"),
-    path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
+    path("edit_blog_post/<str:slug>/", UpdatePostView, name="edit_blog_post"),
     path("delete_blog_post/<str:slug>/", views.Delete_Blog_Post, name="delete_blog_post"),
     path("search/", views.search, name="search"),
 
